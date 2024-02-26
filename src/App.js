bf662dfa614e50b1bfeead89b944a772deaed8e9
Sandeep Kumar Jena <sandeepkumar637053@gmail.com>
2024-02-26 08:32:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import DisplayVideos from './Components/DisplayVideos';
 
 function App() {
+  const [index, setIndex] = useState(0)
+  
+  
+  const videosURL = ["https://player.vimeo.com/progressive_redirect/playback/825038052/rendition/540p/file.mp4?loc=external&oauth2_token_id=57447761&signature=4240d36cf910f709969cc2936efc0ffb19ac5da15543364ef51d08c074de9481", "https://player.vimeo.com/progressive_redirect/playback/910687913/rendition/540p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=2b32cdde960414d71ee080f922eca460a5fb6a4a4fc3c38658239d085bc80ef4", "https://player.vimeo.com/progressive_redirect/playback/910579804/rendition/540p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=5ea7138af95dc72156b3f98df46515e34691608cdf8288df0dfa3dc32cc03ebf", "https://player.vimeo.com/progressive_redirect/playback/911900740/rendition/540p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=e40e590963bee844c840298426daaa5114a84c06500ad9fa096071c0d19ca2b5", "https://player.vimeo.com/progressive_redirect/playback/902458538/rendition/540p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=641298b7c9a70585a57a4517e63eebc3ad6ef8972948da2d98c16ff672f12d2a"]
+
+
+  function keyPressed(e) {
+    if (e.key === "ArrowDown") {
+      if (index < videosURL.length - 1) {
+        setIndex(index + 1)
+      }
+    }
+    else if (e.key === "ArrowUp") {
+      if (index >= 1) {
+        setIndex(index - 1)
+      }
+    }
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div tabIndex={0} onKeyDown={keyPressed} className='h-screen bg-black flex justify-center items-center'>
+       <DisplayVideos url={videosURL[index]} />
     </div>
   );
 }
---
+++ src/Components/DisplayVideos.js
@@ -0,0 +1,97 @@
+import React, { useState, useRef, useEffect } from "react";
+import { IoMdPlay } from "react-icons/io";
+import { IoPauseSharp } from "react-icons/io5";
+import { IoVolumeMute } from "react-icons/io5";
+import { HiMiniSpeakerWave } from "react-icons/hi2";
+import '../App.css';
+
+
+
+function DisplayVideos({url}) {
+    const [paused, setPaused] = useState(false);
+    const [progress, setProgress] = useState(0);
+    const [muted, setMuted] = useState(false);
+    const videoRef = useRef(null);
+
+
+
+    function videoPlayPause() {
+        const video = videoRef.current;
+        if (video.paused) {
+            video.play();
+            setPaused(false);
+        } else {
+            video.pause();
+            setPaused(true);
+        }
+    }
+
+    function updateProgressBar() {
+        const video = videoRef.current;
+        const currentTime = video.currentTime;
+        const duration = video.duration;
+        const progress = (currentTime / duration) * 100;
+        setProgress(progress);
+    }
+
+    useEffect(() => {
+        const video = videoRef.current;
+
+        function updateProgress() {
+            updateProgressBar();
+            if (!video.paused && video.currentTime < video.duration) {
+                requestAnimationFrame(updateProgress);
+            }
+        }
+
+        updateProgress();
+    }, [paused]);
+
+    useEffect(()=>{
+        setPaused(false)
+    },[url])
+
+    
+    function toogleMute() {
+        videoRef.current.muted = !muted
+        setMuted(!muted)
+    }
+    console.log(paused);
+    
+    return (
+         <div className={`w-[350px] h-[580px] rounded-lg overflow-hidden relative`}>
+            <video
+                ref={videoRef}
+                onClick={videoPlayPause}
+                onTimeUpdate={updateProgressBar}
+                src= {url}
+                className="w-full h-full object-cover transition-opacity duration-500 ease-in-out"
+                autoPlay
+                loop
+            ></video>
+
+            {paused ? (
+                <IoMdPlay className="absolute text-white text-2xl top-5 left-5" />
+            ) : (
+                <IoPauseSharp className="absolute text-white text-2xl top-5 left-5" />
+            )}
+
+            <button onClick={toogleMute}>
+                {muted ? (<IoVolumeMute className={`absolute top-5 right-5 text-white text-2xl`}/>
+                ) : (<HiMiniSpeakerWave className={`absolute top-5 right-5 text-white text-2xl`}/>
+                )}
+            </button>
+
+            {/* progressbar */}
+            <div className="w-full h-1 absolute bottom-0 left-0">
+                {/* progressbar inner */}
+                <div
+                    style={{ width: `${progress}%` }}
+                    className="h-full bg-red-600"
+                ></div>
+            </div>
+        </div>
+    );
+}
+
+export default DisplayVideos;
